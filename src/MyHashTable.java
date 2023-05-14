e2b1d6cb36b2641535bbc16b607051add658f5cb
AsimaTitanicCoder <asimamyrzabay@example.com>
2023-05-15 00:58:11 +0900

    // To create a new HashNode with the given key-value pair

---
+++ src/MyHashTable.java
@@ -0,0 +1,117 @@
+
+public class MyHashTable <K, V> {
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        // To create a new HashNode with the given key-value pair
+        private HashNode<K, V> next;
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        @Override
+        public String toString(){
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11; // Defalt node array size
+    private int size;
+
+
+
+    public MyHashTable() {
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[index];
+        chainArray[index] = newNode;
+        size++;
+    }
+    // Returns the value associated with the specified key in the hash table
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+    // Removes the key-value pair associated with the specified key from the hash table
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> prev = null;
+        HashNode<K, V> curr = chainArray[index];
+        while (curr != null) {
+            if (curr.key.equals(key)) {
+                if (prev == null) {
+                    chainArray[index] = curr.next;
+                } else {
+                    prev.next = curr.next;
+                }
+                size--;
+                return curr.value;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
+        return null;
+    }
+    // Return true if the hash table contains the specified value
+    public boolean contains(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+    public K getKey(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null;
+    }
+    public HashNode<K, V>[] getChainArray() {
+        return chainArray;
+    }
+
+}
+
+
